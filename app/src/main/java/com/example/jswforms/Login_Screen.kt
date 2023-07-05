ddd08657d461e36f80021cd6558de7c9e472b091
JSWDOLVI <goodlegiri892@gmail.com>
2023-07-05 17:48:55 +0530

    connecting firebase 16:40

--- app/src/main/java/com/example/jswforms/Login_Screen.kt
+++ app/src/main/java/com/example/jswforms/Login_Screen.kt
@@ -29,7 +29,7 @@ class Login_Screen : AppCompatActivity() {
                      val intent = Intent(this,MainActivity::class.java)
                      startActivity(intent)
                  } else {
-                     Toast.
+
                  }
                 }
             }
