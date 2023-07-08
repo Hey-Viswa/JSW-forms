789c03d0cdac7ab1335c4135652d00eed948e331
JSWDOLVI <goodlegiri892@gmail.com>
2023-07-08 12:19:44 +0530

    signin google

--- app/src/main/java/com/example/jswforms/Homepage/category.kt
+++ app/src/main/java/com/example/jswforms/Homepage/category.kt
@@ -16,7 +16,6 @@ class category : AppCompatActivity() {
     private lateinit var OfficialCategory: CardView
     private lateinit var JobApplication: CardView
     private lateinit var EventCategory: CardView
-    private lateinit var EducationCategory: CardView
     private lateinit var CostumerCategory: CardView
     private lateinit var VendorCategory: CardView
     private lateinit var LogoutibBtn: ImageView
@@ -29,44 +28,38 @@ class category : AppCompatActivity() {
         OfficialCategory = findViewById(R.id.Officialcat)
         JobApplication = findViewById(R.id.catJobApplication)
         EventCategory = findViewById(R.id.Eventcat)
-        EducationCategory = findViewById(R.id.educat)
+
         CostumerCategory = findViewById(R.id.clientcat)
         VendorCategory = findViewById(R.id.vendorcat)
 
 
         OfficialCategory.setOnClickListener {
             val url =
-                "https://docs.google.com/forms/d/1-vuGC4Xwoi6Jc8NfubdGzVcbCmt21daGgrabqKFT1e8/edit"
+                "https://docs.google.com/forms/d/e/1FAIpQLSfRdx856GNoLnL8yoMOqdszFEhZmFM4cbWHorowzWuk5IZPGg/viewform"
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             startActivity(intent)
         }
         JobApplication.setOnClickListener {
             val url =
-               "https://docs.google.com/forms/d/18JISq9GR6V2jOMCjeeQyOoq2RoKSWLlxMvSj3sbwdwE/edit"
+               "https://docs.google.com/forms/d/e/1FAIpQLSfI5uohnvJG5OeyhYYaHRt5SxMsQSDWrPl2F0-SrgiaafKtNg/viewform"
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             startActivity(intent)
         }
         EventCategory.setOnClickListener {
             val url =
-                "https://docs.google.com/forms/u/0/d/1-Bs75BfRhy-uWs-B_Bs6Fwro5DSxzrIWys7Mwo5uk-A/edit?ntd=1&usp=forms_home&ths=true"
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-            startActivity(intent)
-        }
-        EducationCategory.setOnClickListener {
-            val url =
-                "https://docs.google.com/forms/d/1NI_DbnfLX_DPgV_4W2J53AUflvxO1M_HEz_JgeAQR_4/edit"
+                "https://docs.google.com/forms/d/e/1FAIpQLSczGErrq6po4tOf7qkqlTR8H4QNjFasD1MkZAE_yfCNL98xRg/viewform"
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             startActivity(intent)
         }
         CostumerCategory.setOnClickListener {
             val url =
-                "https://docs.google.com/forms/d/1zESTtHRalW47eTJPBCDJLMwS3iheQ1Tbkwe6RhYnYkY/edit"
+                "https://docs.google.com/forms/d/e/1FAIpQLSeVQQXAV4kdSwdzoOFWbCGzADXNHn3HbmdPGtuV_oGqplF2GQ/viewform"
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             startActivity(intent)
         }
         VendorCategory.setOnClickListener {
             val url =
-                "https://docs.google.com/forms/d/1WIhzPXY0eWtcJdsOY6-_BV-Tr9ZN2DupF4pZwdKTIdo/edit"
+                "https://docs.google.com/forms/d/e/1FAIpQLSf5kMXYbU64hJWDDKxT-Br4G-jzdt4D6_F95xsWvgn-ULXpzg/viewform"
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             startActivity(intent)
         }
--- app/src/main/java/com/example/jswforms/Login_and_Register_Activity/Login_Screen.kt
+++ app/src/main/java/com/example/jswforms/Login_and_Register_Activity/Login_Screen.kt
@@ -1,6 +1,7 @@
 package com.example.jswforms.Login_and_Register_Activity
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.text.method.HideReturnsTransformationMethod
@@ -9,23 +10,33 @@ import android.view.MotionEvent
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.jswforms.Homepage.category
 
 import com.example.jswforms.MainActivity.MainActivity
 import com.example.jswforms.R
 import com.example.jswforms.databinding.ActivityLoginScreenBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 
 class Login_Screen : AppCompatActivity() {
 
 
     lateinit var password : EditText
+    lateinit var LoginWithGoogle : SignInButton
+
     var passwordVisible: Boolean = false
     private lateinit var binding : ActivityLoginScreenBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     // Configure sign-in to request the user's ID, email address, and basic
     // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
@@ -39,11 +50,27 @@ class Login_Screen : AppCompatActivity() {
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
         binding = ActivityLoginScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
         password = findViewById(R.id.EtPassword)
+        LoginWithGoogle = findViewById(R.id.LoginWithGoogle)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+        LoginWithGoogle.setOnClickListener{
+            signInGoogle()
+        }
 
         binding.btnLogin.setOnClickListener {
             val email = binding.EtEmail.text.toString()
@@ -94,5 +121,39 @@ class Login_Screen : AppCompatActivity() {
 
 
 
+    }
+    private fun signInGoogle(){
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+        if(result.resultCode == Activity.RESULT_OK){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResult(task)
+        }
+    }
+
+    private fun handleResult(task: Task<GoogleSignInAccount>) {
+       if (task.isSuccessful){
+           val account : GoogleSignInAccount? = task.result
+           if (account != null){
+               updateUi(account)
+           }
+       }else{
+           Toast.makeText(this,task.exception.toString(),Toast.LENGTH_SHORT).show()
+       }
+    }
+
+    private fun updateUi(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
+            if(it.isSuccessful){
+                val intent : Intent = Intent(this@Login_Screen,category::class.java )
+            }else{
+                Toast.makeText(this,it.exception.toString(),Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
