581fc42e6b9ff0d5f8dd699e33d0216138ddc304
JSWDOLVI <goodlegiri892@gmail.com>
2023-07-10 10:53:21 +0530

    sign in and sign up done

--- app/src/main/java/com/example/jswforms/Login_and_Register_Activity/Register_Activity.kt
+++ app/src/main/java/com/example/jswforms/Login_and_Register_Activity/Register_Activity.kt
@@ -1,19 +1,34 @@
 package com.example.jswforms.Login_and_Register_Activity
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.jswforms.Homepage.category
 import com.example.jswforms.R
 import com.example.jswforms.databinding.ActivityRegisterBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class Register_Activity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var ivRegBackbtn: ImageView
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var RegisterWithGoogle : SignInButton
+
+
+
 
 
 
@@ -22,7 +37,7 @@ class Register_Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        RegisterWithGoogle = findViewById(R.id.RegisterWithGoogle)
 
         ivRegBackbtn = findViewById(R.id.ivRegBackbtn)
         ivRegBackbtn.setImageResource(R.drawable.baseline_arrow_back_24)
@@ -32,6 +47,16 @@ class Register_Activity : AppCompatActivity() {
         }
 
         firebaseAuth = FirebaseAuth.getInstance()
+
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+
 
 
         binding.btnRegister.setOnClickListener {
@@ -62,7 +87,43 @@ class Register_Activity : AppCompatActivity() {
             startActivity(loginIntent)
         }
 
+        RegisterWithGoogle.setOnClickListener {
+            signInGoogle()
+        }
 
+    }
+    private fun signInGoogle(){
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+        if(result.resultCode == Activity.RESULT_OK){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResults(task)
+        }
+    }
 
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful){
+            val account : GoogleSignInAccount? = task.result
+            if(account != null){
+                updateUi(account)
+            }
+        }else{
+            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUi(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
+            if(it.isSuccessful){
+                val intent : Intent = Intent(this, category::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
